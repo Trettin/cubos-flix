8c4bc26426a1efc74c826e2a1aa5a14b39c03597
Gabriel Trettin <gtcastro7@gmail.com>
2021-05-13 15:27:17 -0300

    Layout do banner promo pronto

--- src/components/BannerPromo/BannerPromo.js
+++ src/components/BannerPromo/BannerPromo.js
@@ -1,11 +1,30 @@
 import './BannerPromo.css';
 import bannerPromo from '../../assets/images/bg-promotion.svg';
-
+import circleTicket from '../../assets/images/coupon-circle-icon.svg'
+import relogioIcone from '../../assets/images/time-icon.svg'
+import money from '../../assets/images/money.png'
 
 export default function BannerPromo() {
     return(
         <div className='cupom-desconto' style={{background: `url(${bannerPromo}) no-repeat right / cover`}}>
 
+            <div className='aproveite'>
+                <h1>Aproveite Agora</h1>
+                <div className='aproveite-footer'>
+                    <img src={circleTicket} alt="ícone de ticket" />
+                    <span>Cupom: HTMLNAOELINGUAGEM</span>
+                </div>
+            </div>
+
+            <div className='finalizaEm'>
+                <h2>Finaliza em</h2>
+                <div className='finalizaEm-footer'>
+                    <img src={relogioIcone} alt="ícone de relógio" />
+                    <span>{'00'}:{'05'}:{'33'}</span>
+                </div>
+            </div>
+
+            <img className='money' src={money} alt="icone de cédulas de dinheiro" />
 
         </div>
     );
